148ff35a48d6a7500a4b7faadbb9d0ce1590eaac
TheZoq2 <frans.skarman@gmail.com>
2016-08-16 21:11:19 +0200

    Convert tag manager to 'class'

--- files/js/tags.js
+++ files/js/tags.js
@@ -2,6 +2,70 @@ var tag_list = [];
 var tag_input_id = "tag_input";
 var tag_list_id = "tag_list"
 
+
+function tag_manager(tag_list_element, tag_input_element)
+{
+	this.tag_list_element = tag_list_element;
+	this.tag_input_element = tag_input_element;
+
+	this.tag_list = [];
+	
+	//Updates the visual list of tags
+	this.update_tag_dislpay = function()
+	{
+		//Clear the current contents of the list
+		tag_list_element.innerHTML = "";
+
+		//Add the current tags to the list
+		for (var i = 0; i < this.tag_list.length; i++)
+		{
+			add_tag_to_display(tag_list[i]);
+		}
+	}
+
+	//Adds a new tag to the list and updates the list view
+	this.add_tag = function(tag)
+	{
+		var is_duplicate = false;
+		
+		for (var listed_tag in self.tag_list) {
+			if(tag == listed_tag)
+			{
+				return;
+			}
+		}
+
+		self.tag_list.push(tag);
+
+		self.update_tag_dislpay();
+	}
+
+	this.add_tag_to_display = function(tag)
+	{
+		//Creating a new <li> element for the list
+		var li_element = document.createElement("li");
+		li_element.setAttribute("class", "tag_display");
+
+		//Creating a new <div> to contain the contents of the <li>
+		var new_div = document.createElement("div");
+
+		//Insert the new div into the 
+		li_element.insertBefore(new_div, li_element.firstChild);
+		insert_element(new_div, li_element);
+
+		//Add the remove button to the div
+		insert_element(create_remove_button(function(){
+			remove_tag(tag);
+		}), new_div);
+
+		var tag_text = document.createElement("p");
+		tag_text.innerHTML = tag;
+		insert_element(tag_text, new_div);
+
+		//Add the new list element to the tag list
+		insert_element(li_element, this.tag_list_element);
+	}
+}
 //Clears the tag list display and adds all current tags
 function update_tag_dislpay()
 {
@@ -12,7 +76,6 @@ function update_tag_dislpay()
 	{
 		add_tag_to_display(tag_list[i]);
 	}
-	
 }
 function add_tag(tag)
 {
