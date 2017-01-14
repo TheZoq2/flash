cb7c989ee897d03f1d135dcc26260bb0168a75d9
Frans Skarman <frans.skarman@gmail.com>
2017-01-14 11:08:53 +0100

    Change get_files_in_dir to use references

--- src/main.rs
+++ src/main.rs
@@ -45,12 +45,12 @@ use file_database_container::{FileDatabaseContainer};
 
 /**
   Returns a list of all the files in a directory
-  */
-fn get_files_in_dir(dir: String) -> Vec<PathBuf> 
+*/
+fn get_files_in_dir(dir: &String) -> Vec<PathBuf> 
 {
     let mut result = Vec::<PathBuf>::new();
 
-    let full_path = dir + "/*";
+    let full_path = dir.clone() + "/*";
 
     for entry in glob(&full_path).expect("Failed to read glob")
     {
@@ -65,10 +65,11 @@ fn get_files_in_dir(dir: String) -> Vec<PathBuf>
 }
 
 
-fn main() {
-    let target_dir = "/mnt/1TB-files/Pictures/Oneplus".to_string();
-    //let target_dir = "/home/frans/Pictures/imgtest".to_string();
-    let file_list = get_files_in_dir(target_dir.clone());
+fn main() 
+{
+    //let target_dir = "/mnt/1TB-files/Pictures/Oneplus".to_string();
+    let target_dir = "/home/frans/Pictures/imgtest".to_string();
+    let file_list = get_files_in_dir(&target_dir);
 
     let settings = settings::Settings::get_defaults();
 
