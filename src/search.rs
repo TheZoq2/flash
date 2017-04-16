45bc29269546878586ce31b1de374fe64b4af957
Frans Skarman <frans.skarman@gmail.com>
2017-04-16 22:14:11 +0200

    Work on search

--- src/search.rs
+++ src/search.rs
@@ -6,7 +6,7 @@ use regex::Regex;
 
 use file_database::FileEntry;
 
-const TAG_LIST_REGEX: &str = r"of (?P'list'(not ){0,1}\w*(, (not ){0,1}\w*)*( and (not ){0,1}\w*){0,1})";
+const TAG_LIST_REGEX: &str = r"of (?P<list>(not ){0,1}\w*(, (not ){0,1}\w*)*( and (not ){0,1}\w*){0,1})";
 
 /**
   Returns true if the specified file has all of the specified tags
@@ -41,12 +41,14 @@ fn has_some_tag(file: &FileEntry, tags: Vec<String>) -> bool
 pub fn get_tags_from_query(query: &str) -> Vec<String>
 {
     lazy_static!{
-        static ref RE: Regex = Regex::new(TAG_LIST_REGEX).unwrap();
+        static ref TAG_LIST_RE: Regex = Regex::new(TAG_LIST_REGEX).unwrap();
+        static ref AND_RE: Regex = Regex::new(r"\wand\w");
+        static ref TAG_RE: Regex = Regex::new(r"\w+");
     }
 
     //Try to match the search string with the tag list regex template
     //and find the list group
-    let captures = match  RE.captures(query)
+    let captures = match  TAG_LIST_RE.captures(query)
     {
         None => return vec!(),
         Some(v) => v
@@ -58,6 +60,15 @@ pub fn get_tags_from_query(query: &str) -> Vec<String>
         None => return vec!()
     };
 
+    //Replace and with comma
+    let list_str = AND_RE.replace_all(&list_str, ", ");
+
+
+    let mut result = vec!();
+    for cap in TAG_RE.captures_iter(list_str)
+    {
+        result.push(cap);
+    }
     vec!(String::from(list_str))
 }
 
@@ -70,6 +81,9 @@ mod query_tests
     #[test]
     fn query_with_only_tags()
     {
-        assert_eq!(get_tags_from_query("of things and stuff"), vec!("things and stuff"));
+        assert_eq!(get_tags_from_query("of things and stuff"), vec!("things", "stuff"));
+        assert_eq!(get_tags_from_query("of things"), vec!("things"));
+        assert_eq!(get_tags_from_query("of things, stuff and items"),
+                vec!("things", "stuff", "items"));
     }
 }
