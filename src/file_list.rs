48c17a053e2eacc096ad4e85c6c209acf7eab306
TheZoq2 <frans.skarman@gmail.com>
2017-05-24 22:53:16 +0200

    Start migration towards unified file list

--- src/file_list.rs
+++ src/file_list.rs
@@ -5,94 +5,36 @@ use std::option::Option;
 
 
 #[derive(Clone)]
-pub struct File
+enum FileLocation
 {
-    pub path: PathBuf,
-    pub saved_id: Option<i32>
+    Unsaved(PathBuf),
+    Database(i32)
 }
 
 #[derive(Clone)]
 pub struct FileList
 {
-    files: Vec<File>,
-    current_index: usize,
+    files: Vec<FileLocation>,
 }
 
 impl FileList
 {
-    pub fn new(file_paths: Vec<PathBuf>) -> FileList 
+    pub fn from_file_paths(file_paths: Vec<PathBuf>) -> FileList 
     {
         let mut files = vec!();
         for path in file_paths
         {
-            files.push(File{path:path, saved_id: None})
+            files.push(FileLocation::Unsaved(path))
         }
-        
+
         FileList {
             files: files,
-            current_index: 0,
         }
     }
 
-    pub fn get_current_file(&self) -> Option<File>
+    pub fn get(&self, index: usize) -> Option<&FileLocation>
     {
-        if self.current_index < self.files.len()
-        {
-            return Some(self.files[self.current_index].clone());
-        }
-
-        None
-    }
-    pub fn get_current_file_save_id(&self) -> Option<i32>
-    {
-        match self.get_current_file()
-        {
-            Some(val) => val.saved_id,
-            None => None
-        }
-    }
-
-    /**
-      Returns the file after the current file without incrementing the current index. This can
-      be used to preload the images in order to prevent the small lag when loading new images.
-     */
-    pub fn peak_next_file(&self) -> Option<File> 
-    {
-        if self.current_index + 1 < self.files.len()
-        {
-            return Some(self.files[self.current_index + 1].clone());
-        }
-        None
-    }
-    /**
-      Increments current index by one while making sure it doesn't go too far out of bounds
-     */
-    pub fn select_next_file(&mut self)
-    {
-        self.current_index += 1;
-
-        if self.current_index > self.files.len()
-        {
-            self.current_index = self.files.len();
-        }
-    }
-    /**
-      Decrements current index by one while making sure it doesn't go too far out of bounds
-     */
-    pub fn select_prev_file(&mut self)
-    {
-        if self.current_index > 1
-        {
-            self.current_index -= 1;
-        }
-    }
-
-    pub fn mark_current_file_as_saved(&mut self, db_id: i32)
-    {
-        if self.current_index < self.files.len()
-        {
-            self.files[self.current_index].saved_id = Some(db_id);
-        }
+        self.files.get(index)
     }
 }
 
