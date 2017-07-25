15b95d3d9dfd477a23802e9ad28b5eeb8cca4264
TheZoq2 <frans.skarman@gmail.com>
2017-07-25 23:31:00 +0200

    Add api for listing all current file lists

--- src/file_list_response.rs
+++ src/file_list_response.rs
@@ -1,6 +1,7 @@
 use iron::*;
+use persistent::Write;
 
-use file_list::FileListList;
+use file_list::{FileListList, FileList, FileListSource};
 
 use std::sync::{Arc, Mutex};
 
@@ -12,25 +13,28 @@ use serde_json;
 #[derive(Serialize)]
 struct ListResponse {
     pub id: usize,
-    pub length: Option<usize>,
+    pub length: usize,
+    pub source: FileListSource
 }
 
+impl ListResponse
+{
+    pub fn from_file_list(id: usize, file_list: &FileList) -> ListResponse {
+        ListResponse {
+            id,
+            length: file_list.len(),
+            source: file_list.get_source().clone()
+        }
+    }
+}
 
-fn create_file_list_response(file_list_list: Arc<Mutex<FileListList>>, id: usize) -> ListResponse {
+fn create_file_list_response(file_list_list: Arc<Mutex<FileListList>>, id: usize) -> Option<ListResponse> {
     // Fetch the file list
-    let file_amount = {
         let file_list_list = file_list_list.lock().unwrap();
 
-        match file_list_list.get(id) {
-            Some(list) => Some(list.len()),
-            None => None,
-        }
-    };
-
-    ListResponse {
-        id,
-        length: file_amount,
-    }
+        file_list_list.get(id).map(|list| {
+            ListResponse::from_file_list(id, list)
+        })
 }
 
 pub fn reply_to_file_list_request(
@@ -40,5 +44,27 @@ pub fn reply_to_file_list_request(
     let list_response = create_file_list_response(file_list_list, file_list_id);
     Ok(Response::with(
         (status::Ok, serde_json::to_string(&list_response).unwrap()),
+    ))
+}
+
+
+// TODO: This should probably be moved to a more sensible file
+pub fn file_list_listing_handler(request: &mut Request) -> IronResult<Response> {
+    let file_list_list = request.get::<Write<FileListList>>().unwrap();
+
+    reply_to_list_listing_request(file_list_list)
+}
+
+pub fn reply_to_list_listing_request(file_list_list: Arc<Mutex<FileListList>>) -> IronResult<Response> {
+    let file_list_list = file_list_list.lock().unwrap();
+
+    let lists: Vec<ListResponse> = file_list_list.lists_with_ids().iter()
+        .map(|id_list| {
+            let (id, list) = *id_list;
+            ListResponse::from_file_list(id, list)
+        }).collect();
+
+    Ok(Response::with(
+        (status::Ok, serde_json::to_string(&lists).unwrap())
     ))
 }
