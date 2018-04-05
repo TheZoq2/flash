954f56026087517559e05e0362da44ce9a56d180
TheZoq2 <frans.skarman@gmail.com>
2018-04-05 16:02:10 +0200

    Fix linting errors in test_server

--- test/run_sync_test_server.sh
+++ test/run_sync_test_server.sh
@@ -2,13 +2,42 @@
 
 source .env
 
+# Set to 1 if the script should use the enviroment variables for flash parameters
+# rather than the default ones for testing
+# Set to 1 if flash should be run in gdb rather than a orphaned child process. Set
+# this option to debug the remote server
+USE_GDB=0
 
-export DATABASE_URL=$DATABASE_FOREIGN_URL
-export FILE_STORAGE_PATH=/tmp/flash_sync
-export FILE_READ_PATH=test/media/foreign
 export FLASH_PORT=3001
-export LOG_FILE=/tmp/flash_sync/log
+
+# Read commandline parameters
+while getopts ":gp:" opt; do
+    case $opt in
+        g)
+            USE_GDB=1
+            ;;
+        p)
+            FLASH_PORT=${OPTARG}
+            ;;
+        \?)
+            echo "Invalid option: -$opt" >&2
+            echo ""
+            echo " -g run with gdb rather than an orphaned child process"
+            echo " -e use enviroment variables rather than default for testing"
+            exit -1
+            ;;
+    esac
+done
+
+export DATABASE_URL="${DATABASE_FOREIGN_URL}_${FLASH_PORT}"
+export FILE_STORAGE_PATH="/tmp/flash_sync_${FLASH_PORT}"
+export FILE_READ_PATH=test/media/foreign
+
+# Log data to this file
+export LOG_FILE="/tmp/flash_sync/log_${FLASH_PORT}"
+# Use diesel as executable if an alternative is not specified
 export DIESEL_EXE=${DIESEL_EXE:=diesel}
+
 
 # Check if any processes are using the port. Fuser output is weird which is why 2>devnull and sed are needed
 port_pid=$(fuser "${FLASH_PORT}/tcp" 2>/dev/null | sed -e 's/ //g')
@@ -27,30 +56,30 @@ if [ "$port_pid" != "" ] ; then
 fi
 
 # Create storage dir
-if ! mkdir -p ${FILE_STORAGE_PATH} > /dev/null; then
+if ! mkdir -p "${FILE_STORAGE_PATH}" > /dev/null; then
     echo "Folder creation failed"
     exit -1
 fi
 
 # Clear the folder target folder if it is non-empty
-if [ -z "$(ls ${FILE_STORAGE_PATH})" ]; then : ; else
+if [ -z "$(ls "${FILE_STORAGE_PATH}")" ]; then : ; else
     if ! rm -r ${FILE_STORAGE_PATH:?}/* > /dev/null; then
         echo "Failed to remove content of storage folder"
         exit -1
     fi
 fi
 
-echo "Database: $DATABASE_URL" >> $LOG_FILE
+echo "Database: $DATABASE_URL" >> "$LOG_FILE"
 
 
 # Set up database
-if ! diesel database reset >> $LOG_FILE; then
+if ! diesel database reset >> "$LOG_FILE"; then
     echo "Database setup failed"
-    cat $LOG_FILE
+    cat "$LOG_FILE"
     exit -1
 fi
 
-if [ "$1" == "" ]; then
+if [[ $USE_GDB -ne 1 ]]; then
     # Create a fifo file for writing the output of flash
     FIFO_NAME=/tmp/flash_sync/fifo
     mkfifo $FIFO_NAME
@@ -69,9 +98,6 @@ if [ "$1" == "" ]; then
     until grep -m 1 "ready" $FIFO_NAME > /dev/null; do sleep 0.1; done
 
     echo "{\"pid\": \"$FLASH_PID\"}"
-elif [ "$1" == "gdb" ]; then
-    gdb target/debug/flash
 else
-    echo "Unrecognised subcommand: $1, expected '' or gdb"
-    exit -1
+    gdb target/debug/flash
 fi
